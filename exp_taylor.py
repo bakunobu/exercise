b0ced8babf30262e740cd3a86117bb551816eb89
Sergei Vakunov <bakunobu@gmail.com>
2020-10-13 21:16:41 +0300

    recursive func added

--- exp_taylor.py
+++ exp_taylor.py
@@ -5,15 +5,22 @@
 
 import math
 
-def exp_tailor(x: float, n: int) -> int:
+def exp_taylor(x: float, n: int) -> int:
     total = 1 + x
     for i in range(2, n+1):
         total += x ** i / math.factorial(i)
     return(total)
 
 
+def taylor_ser_rec(x: float, n: int) -> float:
+    if n == 0:
+        return(1)
+    while n > 0:
+        return(math.pow(x, n) / math.factorial(n) + taylor_ser_rec(x, n-1))
+
+
 # testing
-eps = 10e-6
+eps = 10e-6 # epsilon
 print(abs(exp_tailor(3, 100) - math.exp(3)) <= eps)
 
 
