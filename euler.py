ff82cffa40302936c068518f949e46d7a147349d
Sergei Vakunov <bakunobu@gmail.com>
2020-10-16 21:56:55 +0300

    different approach, still doesnt work

--- euler.py
+++ euler.py
@@ -1,36 +1,23 @@
 from datetime import datetime
 start = datetime.now()
 
-pos_e = [e ** 5 for e in range(150, 5, -1)]
-
-pos_d = [d ** 5 for d in range(149, 4, -1)]
-
-pos_c = [c ** 5 for c in range(148, 3, -1)]
-
-pos_b = [b ** 5 for b in range(147, 2, -1)]
-
-pos_a = [a ** 5 for a in range(146, 1, -1)]
+pos_e = [e ** 5 for e in range(150, 1, -1)]
 
 
-results = []
-while True:
-    for d in pos_d:
-        if len(results) != 0:
-            break
-        for c in pos_c:
-            if len(results) != 0:
-                break
-            for b in pos_b:
-                if len(results) != 0:
-                    break
-                for a in pos_a:
-                    if a + b + c + d in pos_e:
-                        results.append(int(((a + b + c + d) ** 0.2)))
-                        break
+
+
+def find_diffs(my_list):
+    diffs = [b for a in my_list for b in my_list if (a - b) in my_list]
+    print(diffs)
+
+    return(diffs)
+
+
+result = find_diffs(pos_e)
+# for _ in range(4):
+#     result = find_diffs(result)
                         
-                    
-
 
 delta = start - datetime.now()
-print(results[0])
+print(result)
 print(f'Duration {delta}')
