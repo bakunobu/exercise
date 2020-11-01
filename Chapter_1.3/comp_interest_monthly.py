467fd8b81d7f52f48ba1341b061745e279d62c95
Sergei Vakunov <bakunobu@gmail.com>
2020-11-01 23:46:42 +0300

    'monthly payment'

---
+++ Chapter_1.3/comp_interest_monthly.py
@@ -0,0 +1,48 @@
+""" Модифицируйте свое решение упражнения о непрерывно начисляемом сложном проценте из раздела так,
+чтобы оно выво­дило таблицу с общей суммой выплат и остающейся основной суммой вклада после каждой ежемесячной выплаты.  """
+
+def calc_anuuity_monthly(K: float, P: float) -> float:
+    """
+    Calculates an annuity
+    
+    Args:
+    =====
+    P: float
+    a principal
+    K: float
+    an annuity
+    
+    Return:
+    =======
+    x: float
+    a monthly payment
+    """
+    x = K * P
+    return(x)
+
+
+def credit_calc(P: float, i: float, n: int) _-> None:
+    """
+    A credit calculator for annual payments
+    
+    Args:
+    =====
+    P: float
+    a principal
+    
+    i: float
+    an interest rate (yearly)
+    
+    n: int
+    period (in months)
+    
+    Return:
+    =======
+    None: None
+    prints a table
+    """
+    
+    i_m = i / 12
+    K = (i_m * (1 + i_m)**n) / ((1 + i_m) ** n - 1)
+    x = K * P
+    
