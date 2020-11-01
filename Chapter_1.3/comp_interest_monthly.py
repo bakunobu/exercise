094f8228266a0d333ce6c0e3b5fc1f3ccf9cdecc
Sergei Vakunov <bakunobu@gmail.com>
2020-11-02 00:06:15 +0300

    'annuity func added'

--- Chapter_1.3/comp_interest_monthly.py
+++ Chapter_1.3/comp_interest_monthly.py
@@ -21,6 +21,30 @@ def calc_anuuity_monthly(K: float, P: float) -> float:
     return(x)
 
 
+def calc_annuity(i: float, n: int) -> float:
+    """
+    Calculates annuity
+    
+    Args:
+    ====
+    i: float
+    an interest rate (yearly)
+    n: int
+    period (in months)
+    
+    Return:
+    =======
+    K: float
+    an annuity
+    
+    """
+    i_m = i / 12
+    expr_1 = (i_m * (1 + i_m)**n)
+    expr_2 = (1 + i_m) ** n - 1
+    K = expr_1 / expr_2
+    return(K)
+
+
 def credit_calc(P: float, i: float, n: int) _-> None:
     """
     A credit calculator for annual payments
