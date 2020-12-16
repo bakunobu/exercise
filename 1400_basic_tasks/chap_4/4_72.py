e2749fe4ab2d0f4d898f92e1e1dce540b16ab029
bakunobu <bakunobu@gmail.com>
2020-12-16 12:37:27 +0300

    '12/16 morning practice'

--- 1400_basic_tasks/chap_4/4_72.py
+++ 1400_basic_tasks/chap_4/4_72.py
@@ -37,8 +37,34 @@ def is_in_one_of_rect(x_1_a, y_1_a, len_x_a, len_y_a,
     return(cond1 or cond2)
 
 # c
+
+def cross_lines(x_1_a:float, y_1_a:float,
+                x_2_a:float, y_2_a:float,
+                x_1_b:float, y_1_b:float,
+                x_2_b:float, y_2_b:float) -> bool:
+    if x_1_a == x_2_a:
+        cond1 = min(y_1_a, y_2_a) < y_1_b < max(y_1_a, y_2_a)
+        cond2 = min(x_1_b, x_2_b) < x_1_a < max(x_1_b, x_2_b)
+    else:
+        cond1 = min(y_1_b, y_2_b) < y_1_a < max(y_1_b, y_2_b)
+        cond2 = min(x_1_a, x_2_a) < x_1_b < max(x_1_a, x_2_a)
+    return(cond1 and cond2)
+    
+    
 def is_crossing(x_1_a, y_1_a, len_x_a, len_y_a,
                 x_1_b, y_1_b, len_x_b, len_y_b) -> bool:
     fig_a = find_all_points(x_1_a, y_1_a, len_x_a, len_y_a)
     fig_b = find_all_points(x_1_b, y_1_b, len_x_b, len_y_b)
-    pass
+    points_a = [(x, y) for x, y in zip(fig_a[:4], fig_a[4:])]
+    points_b = [(x, y) for x, y in zip(fig_b[:4], fig_b[4:])]
+    points_b = points_b[1:] + points_b[0]
+    for i in range(3):
+        if cross_lines(points_a[i][0], points_a[i][1],
+                       points_a[i+1][0], points_a[i+i][1],
+                       points_b[i][0], points_b[i][1],
+                       points_b[i+1][0], points_b[i+1][1]):
+            return(True)
+    return(False)
+
+    
+        
