466587d551373df162a129996050b6ffb5c79d48
bakunobu <bakunobu@gmail.com>
2021-03-08 14:38:39 +0300

    '03/08 practice'

---
+++ 1400_basic_tasks/chap_8/ex_8_4.py
@@ -0,0 +1,23 @@
+from fractions import Fraction
+
+
+def calc_part() -> None:
+    
+    while True:
+        try:
+            a = float(input('Введите число: '))
+            if 0 < a <= 1:
+                break
+            else:
+                print('Число должно принадлежать интервалу от 0 до 1!')
+        except ValueError:
+            print('используйте десятичные дроби с разделителем - .')
+    
+    i = 1
+    
+    while Fraction(1 , i) >= a:
+        i += 1
+        
+    print(Fraction(1, i))
+        
+calc_part()
