d63343ca361be844655da724ac28999a1a0191ad
bakunobu <bakunobu@gmail.com>
2021-03-14 22:32:08 +0300

    '03/14 practice'

---
+++ 1400_basic_tasks/chap_9/ex_9_2.py
@@ -0,0 +1,12 @@
+# a
+for x in range(1, 10):
+    for y in range(1, 10):
+        print(f'{x}+{y}={x+y}', end=' ')
+    print()
+    
+print('* * *')
+# b
+for x in range(1, 10):
+    for y in range(1, 10):
+        print(f'{y}+{x}={x+y}', end=' ')
+    print()
