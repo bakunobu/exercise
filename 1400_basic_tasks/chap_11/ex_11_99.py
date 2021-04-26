329afa2eec3035b217db9f6dd48a30094b11eba6
bakunobu <bakunobu@gmail.com>
2021-04-26 21:13:53 +0300

    '04/25 morning var names fixed'

--- 1400_basic_tasks/chap_11/ex_11_99.py
+++ 1400_basic_tasks/chap_11/ex_11_99.py
@@ -1,5 +1,5 @@
 def le_mean_grade(grade:mean) -> None:
     g = sum(grade) / len(grade)
     for i in range(len(grade)):
-        if grade[i] < m:
+        if grade[i] < g:
             print(i + 1)
