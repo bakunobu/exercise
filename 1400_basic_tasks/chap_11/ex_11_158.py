5bf71b2b4e08b67bce5fa5a2cc4d39ecb5590c13
bakunobu <bakunobu@gmail.com>
2021-05-14 23:10:48 +0300

    '05/14 practice'

---
+++ 1400_basic_tasks/chap_11/ex_11_158.py
@@ -0,0 +1,12 @@
+# a
+def del_max(nums:list) -> list:
+    nums.remove(max(nums))
+    
+    return(nums)
+
+
+# b
+def del_min(nums:list) -> list:
+    nums.remove(min(nums))
+    
+    return(nums)
